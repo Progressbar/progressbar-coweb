3e0e79928459b2146adf4739474f7c44b43bb7ba
Matej Nemcek <ybdaba@gmail.com>
2017-09-11 14:04:57 +0200

    smaller VR button

--- cowork-panel/src/components/Subscribe.vue
+++ cowork-panel/src/components/Subscribe.vue
@@ -200,7 +200,7 @@
 </div>
 <div class="columns is-mobile">
   <div class="column is-narrow">
-    <a class="button is-large is-outlined is-light" href="#">I want to see the workspace (VR)</a>
+    <a class="button is-medium is-outlined is-light" href="#">I want to see the workspace (VR)</a>
   </div>
 </div>
 </div>
