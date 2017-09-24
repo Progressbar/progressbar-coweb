23f3dff456cf59e47cf3d59900c904dd69dfea23
Matej Nemcek <ybdaba@gmail.com>
2017-09-24 22:54:17 +0200

    status in stdlib

---
+++ stdlib-workspace/yangwao/status/functions/__main__.js
@@ -0,0 +1,45 @@
+/**
+* A basic Hello World function
+* @param {string} name Who you're saying hello to
+* @returns {object}
+*/
+module.exports = (name = 'world', context, callback) => {
+  const firebase = require('firebase-admin')
+  const firebaseConfig = {
+    type: process.env.firebase_type,
+    project_id: process.env.firebase_project_id,
+    private_key_id: process.env.firebase_private_key_id,
+    private_key: process.env.firebase_private_key,
+    client_email: process.env.firebase_client_email,
+    client_id: process.env.firebase_client_id,
+    auth_uri: process.env.firebase_auth_uri,
+    token_uri: process.env.firebase_token_uri,
+    auth_provider_x509_cert_url: process.env.firebase_auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.firebase_client_x509_cert_url
+  }
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp({
+      credential: firebase.credential.cert(firebaseConfig),
+      databaseURL: "https://coweb-bc478.firebaseio.com"
+    })
+  }
+
+  const db = firebase.database();
+  const ref = db.ref("server")
+  const subscribersRef = ref.child("subscribers")
+
+  ref.once('value', function (data) {
+    let server = data.val()
+    let seats = {
+      subscribers: Object.keys(server.subscribers).length,
+      free: 10,
+      waitlist: 2,
+      capacity: 14,
+      allocatedToday: 0
+    }
+    let status = {
+      seats,
+    }
+    callback(null, status);
+  })
+};
