82fcfbde4b4219a86cc03a5386f44e7f4e353b68
Matej Nemcek <ybdaba@gmail.com>
2017-10-12 21:59:38 +0200

    localdoor opening script

--- localdoor/open.js
+++ localdoor/open.js
@@ -1,7 +1,29 @@
 const firebase = require('firebase-admin')
-let serviceAccount = require("path/to/serviceAccountKey.json")
+let serviceAccount = require('./pKey.json')
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://coweb-bc478.firebaseio.com"
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: 'https://coweb-bc478.firebaseio.com'
 })
+
+const db = firebase.database()
+const localdoor = db.ref('localdoor')
+let doorLock = true
+
+localdoor.on('child_added', function(data) {
+  if (doorLock) {
+    openDoor()
+    console.log(`${Date()} openning for ${data.val()}`)
+  }
+}, function(error) {
+  console.log(`err ${err.code}`)
+})
+
+function openDoor() {
+  doorLock = false
+  console.log(`openning door pin`)
+  setTimeout(function () {
+    console.log(`closing door pin`)
+    doorLock = true
+  }, 1000);
+}
