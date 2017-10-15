66bfede71656b9fcd21a049ff4d39bbf8bb0e721
Matej Nemcek <ybdaba@gmail.com>
2017-10-15 21:30:18 +0200

    gpio fix

--- localdoor/gpio.js
+++ localdoor/gpio.js
@@ -2,11 +2,7 @@
 // echo out > /sys/class/gpio/gpio23/direction
 // echo 1 > /sys/class/gpio23/value
 
-var Gpio = require('onoff').Gpio,
-  led = new Gpio(23, 'out'),
-  button = new Gpio(23, 'in', 'both');
+let Gpio = require('onoff').Gpio
+let led = new Gpio(23, 'out')
 
- led.writeSync(1);
-// button.watch(function(err, value) {
-//
-// });
+led.writeSync(1);
