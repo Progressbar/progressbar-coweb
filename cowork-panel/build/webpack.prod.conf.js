148bff2de343b33583ed25945784d3ff3fcf992e
Matej Nemček <ybdaba@gmail.com>
2017-11-12 00:58:47 +0100

    modified service worker cache for production

--- cowork-panel/build/webpack.prod.conf.js
+++ cowork-panel/build/webpack.prod.conf.js
@@ -100,7 +100,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'my-vue-app',
       filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      staticFileGlobs: ['dist/**/*.{js,html,css,svg,jpg}'],
       minify: true,
       stripPrefix: 'dist/'
     })
