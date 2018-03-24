beb230e4a8b272f5ecdc589261d7c314573e09c9
Matej Nemček <ybdaba@gmail.com>
2018-03-24 12:23:04 +0100

    removed workspace vr buttons from main page

--- cowork-panel/src/components/Subscribe.vue
+++ cowork-panel/src/components/Subscribe.vue
@@ -232,17 +232,6 @@
   </div>
   <div class="columns is-centered">
     <div class="column is-narrow">
-      <a class="button is-outlined is-white" href="https://www.google.com/maps/place/Progressbar+hackerspace/@48.144557,17.1154906,3a,75y,0.9h,86.87t/data=!3m8!1e1!3m6!1sAF1QipNURKoHKNuKcFfvtybFoysAvtsQ870hhGEGxyqh!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNURKoHKNuKcFfvtybFoysAvtsQ870hhGEGxyqh%3Dw203-h100-k-no-pi-0-ya31.000006-ro-0-fo100!7i8000!8i4000!4m5!3m4!1s0x476c8946152ff129:0x72ac0b6fccaf66fd!8m2!3d48.144557!4d17.1154906" >Workspace VR Daylight (360)</a>
-    </div>
-    <div class="column is-narrow">
-      <a class="button is-outlined is-white" href="https://www.google.com/maps/place/Progressbar+hackerspace/@48.144557,17.1154906,3a,75y,92.83h,78.38t/data=!3m8!1e1!3m6!1sAF1QipNPdefRErb2zp2wCMPCUEkOBJKDkcZcRn6ls4D0!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNPdefRErb2zp2wCMPCUEkOBJKDkcZcRn6ls4D0%3Dw203-h100-k-no-pi0-ya0-ro0-fo100!7i8000!8i4000!4m5!3m4!1s0x476c8946152ff129:0x72ac0b6fccaf66fd!8m2!3d48.144557!4d17.1154906">Workspace VR Evening (360)</a>
-    </div>
-    <div class="column is-narrow">
-      <router-link to="/coworking-bratislava"><a class="button is-outlined is-white">Coworking Bratislava</a></router-link>
-    </div>
-  </div>
-  <div class="columns is-centered">
-    <div class="column is-narrow">
       <h1 id="blackh1">Tech Cowork Progressbar in Bratislava</h1>
     </div>
   </div>
