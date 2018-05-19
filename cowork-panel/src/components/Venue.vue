fcda830c94bebf9ca10a5b28f3d3dac4d2c3d10f
Matej Nemček <ybdaba@gmail.com>
2018-05-19 15:26:39 +0200

    upgraded venue text

--- cowork-panel/src/components/Venue.vue
+++ cowork-panel/src/components/Venue.vue
@@ -3,15 +3,25 @@
   <div class="container is-fluid">
     <div class="columns is-centered">
       <div class="column is-12 content">
-        <p class="has-text-white">
-          We are located over  Black King Bar on the first floor. You can access our space through doors next to Black King Bar. During event, ring bell (on the left side) Progressbar and probably somebody will open to you.
+        <h2 class="has-text-white">
+          We are located over Black King Bar on the first floor. <br />
+          You can access our space through doors next to Black King Bar. <br />
+          During event, ring 🛎 bell (on the left side) Progressbar and probably somebody will open to you.<br />
+          If you plan visit us during day, contact us ahead 🤗 Thanks
+        </h2>
+        <h3 class="has-text-white">
+          Address <br />
+          Progressbar hackerspace,
+          Dunajská 7495/14, 811 08 Bratislava
+          </h3>
+        <p>
+          <a href="https://www.openstreetmap.org/node/1096120193">Progressbar at Openstreetmap</a>
+          ||
+          <a href="https://www.google.com/maps/place/Progressbar+hackerspace/@48.144557,17.1133019,17z/data=!3m1!4b1!4m5!3m4!1s0x476c8946152ff129:0x72ac0b6fccaf66fd!8m2!3d48.144557!4d17.1154906">Progressbar at Google Maps</a>
         </p>
         <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=17.113958001136783%2C48.143727459609195%2C17.116854786872867%2C48.14537578584775&amp;layer=mapnik&amp;marker=48.14455162934517%2C17.115406394004822"
           style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=48.14455&amp;mlon=17.11541#map=19/48.14455/17.11541&amp;layers=N">View Larger Map</a></small>
-        <p><a href="https://www.openstreetmap.org/node/1096120193">Progressbar at Openstreetmap</a></p>
-        <p>
-          <a href="https://www.google.com/maps/place/Progressbar+hackerspace/@48.144557,17.1133019,17z/data=!3m1!4b1!4m5!3m4!1s0x476c8946152ff129:0x72ac0b6fccaf66fd!8m2!3d48.144557!4d17.1154906">Progressbar at Google Maps</a>
-        </p>
+
         <div class="columns">
           <div class="column is-3">
             <img src="/static/img/IMG_20180403_185405_1.jpg" alt="dunajska 14 progressbar" />
