bb2c597d06eccf11640a3d4b5bb9c58c07d45abd
Ivkaa <ivanka.netopierik18@gmail.com>
2018-12-16 15:56:08 +0100

    Add grid to ig feed

--- web/src/components/IgFeed/IgFeed.jsx
+++ web/src/components/IgFeed/IgFeed.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./IgFeed.css";
+import { Grid, Image } from 'semantic-ui-react'
 
 class IgFeed extends Component {
   constructor() {
@@ -10,7 +11,8 @@ class IgFeed extends Component {
   }
 
   async componentDidMount () {
-    const url = 'https://wt-4665476769bf9081da4dd878527b9c7d-0.sandbox.auth0-extend.com/pb-web-ig'
+    const count = 10
+    const url = `https://wt-4665476769bf9081da4dd878527b9c7d-0.sandbox.auth0-extend.com/pb-web-ig?count=${count}`
     const igData = await fetch(url, {
         method: "GET",
     }).then(res => res.json())
@@ -21,11 +23,13 @@ class IgFeed extends Component {
     const { igPosts } = this.state
     return (
       <div>
-        {igPosts.map(post => (
-          <div key={post.id}>
-            <img src={post.images.standard_resolution.url} alt={post.tags} />
-          </div>
-        ))}
+        <Grid columns={4}>
+          {igPosts.map(post => (
+            <Grid.Column>
+              <Image src={post.images.standard_resolution.url} alt={post.tags} />
+            </Grid.Column>
+          ))}
+        </Grid>
       </div>
     )
   }
