e5f88752eb3f32c1f9312a51bfa335cb661290db
Viktor Valastin <viktorko99@gmail.com>
2019-01-12 16:36:15 +0100

    added iframe wrapper component

---
+++ cowork-panel/src/components/ImageFrame.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <iframe :src="this.url" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+</template>
+
+<script>
+    export default {
+        name: 'ImageFrame',
+        props: ['url'],
+    }
+</script>
+
+<style scoped>
+
+  @media only screen and (max-width: 768px) {
+    iframe {
+      width: 85vw;
+    }
+  }
+
+  iframe {
+    width: 100%;
+  }
+
+</style>
