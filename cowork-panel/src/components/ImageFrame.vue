05202b409a156b46cb6129ce73a1abae6c57fb52
Matej Nemček <ybdaba@gmail.com>
2019-01-16 15:45:44 +0100

    trying dns-prefetch for imageFrame

--- cowork-panel/src/components/ImageFrame.vue
+++ cowork-panel/src/components/ImageFrame.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <iframe :src="this.url" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe rel="dns-prefetch" :src="this.url" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 </template>
 
