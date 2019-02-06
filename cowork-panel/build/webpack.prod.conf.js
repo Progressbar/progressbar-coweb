5244cc451c46bd9db8844c867c9679e117c1c702
Viktor Valastin <viktorko99@gmail.com>
2019-02-06 10:21:43 +0100

    maybe solution for redirects episode 8000

--- cowork-panel/build/webpack.prod.conf.js
+++ cowork-panel/build/webpack.prod.conf.js
@@ -97,9 +97,14 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../root'),
+        to: config.build.assetsRoot,
+        ignore: ['.*']
       }
     ]),
-    new ImageminPlugin({ 
+    new ImageminPlugin({
       test: /\.(jpe?g|png|gif|svg)$/i,
       plugins: [
         imageminMozjpeg({
