d1aa01ec187d9d11f698933eb4c1d606f49a1421
Matej Nemček <ybdaba@gmail.com>
2019-02-14 14:44:39 +0100

    forgot add component

---
+++ cowork-panel/src/components/ProgressbarLogoIcon.vue
@@ -0,0 +1,12 @@
+<template>
+<img src="/static/img/icons/android-launchericon-144-144.png">
+</template>
+
+<script>
+export default {
+  name: 'progressbar-logo-icon',
+}
+</script>
+
+<style scoped>
+</style>
