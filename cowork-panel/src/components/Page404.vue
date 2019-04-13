4594522fe90b9d7d99ece0ae591838483d69dc0c
Matej Nemček <ybdaba@gmail.com>
2019-04-13 23:33:27 +0200

    fixed 404 page

--- cowork-panel/src/components/Page404.vue
+++ cowork-panel/src/components/Page404.vue
@@ -1,6 +1,12 @@
 <template>
 <div class="Page404">
-  Page404
+  <div class="container is-fluid">
+    <div class="column is-8 content has-text-white">
+      <h1 class="has-text-white">
+        Looks like you've just found end of our webpage, anyway you are welcome to come over to our place 😋
+      </h1>
+    </div>
+  </div>
 </div>
 </template>
 
