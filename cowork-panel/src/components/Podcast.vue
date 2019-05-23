f4ed35c654bb7da981f09570a4582220c3a1560b
Matej Nemček <ybdaba@gmail.com>
2019-05-23 14:40:51 +0200

    added links to podcast pppp

--- cowork-panel/src/components/Podcast.vue
+++ cowork-panel/src/components/Podcast.vue
@@ -5,6 +5,17 @@
         <iframe height="600" src="https://www.youtube.com/embed/videoseries?list=PLfCtn83lMV3CzkXkGdkdUnNZvD5c4hBbC" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
+    <div class="columns is-centered">
+      <div class="column is-12 content">
+        <h1 class="">
+        <a href="https://cowork.progressbar.sk/podcast">Prvý Pokusný Podcast Progressbaru - PPPP - 🔗🔗🔗 links</a><br><br>
+        🎥 <a href="https://www.youtube.com/playlist?list=PLfCtn83lMV3CzkXkGdkdUnNZvD5c4hBbC">Youtube Playlist</a><br>
+        🎧 <a href="https://open.spotify.com/show/2arFgMfH8p27iEpwJmFv23">Spotify</a><br>
+        🐬 <a href="https://api.substack.com/feed/podcast/10023.rss">RSS</a><br>
+        📧 <a href="https://pppp.substack.com/">Newsletter</a>
+        </h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,5 +48,7 @@
     iframe {
       width: 100%;
     }
-
+  a {
+    color: hotpink;
+  }
 </style>
