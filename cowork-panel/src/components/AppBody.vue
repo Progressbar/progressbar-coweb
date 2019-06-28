b4892fe85bf0074a209faa21fc70d848d48bed2d
Matej Nemček <ybdaba@gmail.com>
2019-06-28 14:10:01 +0200

    hackerspace -> community space

--- cowork-panel/src/components/AppBody.vue
+++ cowork-panel/src/components/AppBody.vue
@@ -10,7 +10,7 @@
               <h1 class="title">
                 <progressbar-logo-refresh />
               </h1>
-              <h2 class="subtitle">✨Your favourite blockchain cowork, hackerspace and hangout zone in <a href="https://goo.gl/maps/gtmznEkBVGG2">downtown, Bratislava</a> 👾</h2>
+              <h2 class="subtitle">✨Your favourite blockchain cowork, community space and hangout zone in <a href="https://goo.gl/maps/gtmznEkBVGG2">downtown, Bratislava</a> 👾</h2>
             </div>
             <div class="column">
               <div class="content has-text-centered">
