3fdc7e364672de660f0f841a03c9abcd972f970d
Matej Nemček <ybdaba@gmail.com>
2019-11-28 15:37:24 +0100

    new team photos, adjusted soft-404

--- cowork-panel/src/components/Page404.vue
+++ cowork-panel/src/components/Page404.vue
@@ -1,11 +1,33 @@
 <template>
 <div class="Page404">
   <div class="container is-fluid">
-    <div class="column is-10 content has-text-white">
-      <h1 class="has-text-white">
-        Looks like you've just found end of our coworking webpage.<br>
-        You are <router-link to="/contact-venue">welcome to come over for free day pass 😋</router-link>
-      </h1>
+    <div class="columns is-10 content has-text-white">
+      <div class="column is-8">
+        <img src="/static/img/cowork/making_movie_cowork.jpg" alt="coworking bratislava making movie">
+      </div>
+      <div class="column is-4">
+        <div class="content">
+          <h1 class="has-text-white">
+            <p>Looks like you've just found end of our coworking webpage.</p>
+            <p>You are <router-link to="/contact-venue">welcome to come over for free day pass 😋</router-link></p>
+          </h1>
+          <p>🎯 Space for focused work</p>
+          <p>🙌
+            <router-link to="rent-flex-desk-coworking-bratislava">
+            Space for collaborative work
+            </router-link>
+          </p>
+          <p>🛋 Space for relaxation</p>
+          <p>🍷 Community Social Events</p>
+          <p>⚡️ Ultra-fast Internet</p>
+          <p>💬 Hangout areas</p>
+          <p>🔐 Private call room</p>
+          <p>🤝
+            <router-link to="meeting-room-cowork-bratislava">Meeting rooms
+            </router-link>
+          </p>
+        </div>
+      </div>
     </div>
     <Offer />
   </div>
--- cowork-panel/src/components/Team.vue
+++ cowork-panel/src/components/Team.vue
@@ -2,28 +2,38 @@
   <div class="Team">
     <div class="columns is-centered">
       <div class="column is-4 content has-text-white">
-        <img width="180px" src="/static/img/team/matej_nemcek.jpg">
+        <img width="320px" src="/static/img/team/matej_nemcek.jpg">
         <h3 class="has-text-white">
           Matej Nemček
         </h3>
         <p>
           Founder & Growth
         </p>
-        <span class="social-icons" v-for="item in socialsYangwao" :key="item.url">
+        <span class="social-icons" v-for="item in socialsYangwao.slice(0,3)" :key="item.url">
+          <a :href="item.url" target="_blank" rel="noopener" class="social-icon" aria-hidden="true">
+            <font-awesome-icon v-if="item.icon" :icon="item.icon" />
+          </a>
+        </span><br>
+        <span class="social-icons" v-for="item in socialsYangwao.slice(3,6)" :key="item.url">
           <a :href="item.url" target="_blank" rel="noopener" class="social-icon" aria-hidden="true">
             <font-awesome-icon v-if="item.icon" :icon="item.icon" />
           </a>
         </span>
       </div>
       <div class="column is-4 content has-text-white">
-        <img width="180px" src="/static/img/team/anett_rolikova.jpg">
+        <img width="320px" src="/static/img/team/anett_rolikova.jpg">
         <h3 class="has-text-white">
           Anett Rolikova
         </h3>
         <p>
-          Operations & Partnerships
+          Events & Operations
         </p>
-        <span class="social-icons" v-for="item in socialsAnett" :key="item.url">
+        <span class="social-icons" v-for="item in socialsAnett.slice(0,3)" :key="item.url">
+          <a :href="item.url" target="_blank" rel="noopener" class="social-icon" aria-hidden="true">
+            <font-awesome-icon v-if="item.icon" :icon="item.icon" />
+          </a>
+        </span><br>
+        <span class="social-icons" v-for="item in socialsAnett.slice(3,6)" :key="item.url">
           <a :href="item.url" target="_blank" rel="noopener" class="social-icon" aria-hidden="true">
             <font-awesome-icon v-if="item.icon" :icon="item.icon" />
           </a>
@@ -54,18 +64,18 @@ export default {
           instagram: 'https://www.instagram.com/progressbar_sk/'
         },
         socialsYangwao: [
-          { url: 'https://www.instagram.com/yangwao', title: 'Instagram', icon: faInstagram },
+          { url: 'https://twitter.com/yangwao', title: 'Twitter', icon: faTwitterSquare },
           { url: 'https://www.linkedin.com/in/mnemcek/', title: 'Linkedin', icon: faLinkedin },
           { url: 'https://www.facebook.com/matej.wao.nemcek', title: 'Facebook', icon: faFacebook },
-          { url: 'https://twitter.com/yangwao', title: 'Twitter', icon: faTwitterSquare },
+          { url: 'https://www.instagram.com/yangwao', title: 'Instagram', icon: faInstagram },
           { url: 'https://t.me/yangwao', title: 'Telegram', icon: faTelegram },
           { url: 'https://m.me/matej.wao.nemcek', title: 'Messenger', icon: faFacebookMessenger }
         ],
         socialsAnett: [
-          { url: 'https://www.instagram.com/anett.rolikova', title: 'Instagram', icon: faInstagram },
+          { url: 'https://twitter.com/anettrolikova', title: 'Twitter', icon: faTwitterSquare },
           { url: 'https://www.linkedin.com/in/anette-rolikova', title: 'Linkedin', icon: faLinkedin },
           { url: 'https://www.facebook.com/anett.rolikova', title: 'Facebook', icon: faFacebook },
-          { url: 'https://twitter.com/anettrolikova', title: 'Twitter', icon: faTwitterSquare },
+          { url: 'https://www.instagram.com/anett.rolikova', title: 'Instagram', icon: faInstagram },
           { url: 'https://t.me/anettrolikova', title: 'Telegram', icon: faTelegram },
           { url: 'https://m.me/anett.rolikova', title: 'Messenger', icon: faFacebookMessenger }
 
